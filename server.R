191a96d8a6f5f9c98eb5441661e1ac5f0c535344
Sander Wuyts <wuyts@embl.de>
2020-09-07 09:35:30 +0200

    Small fixes + new obosolete names

--- server.R
+++ server.R
@@ -42,31 +42,16 @@ shinyServer(function(input, output) {""
             return(HTML(welcome_text))
         }
         
-        # Get name
-        species <- search$filtered_table %>% pull(new_name) %>% str_c("<i>", ., "</i>")
-        # Get type strains
-        typestrain <- search$filtered_table %>% pull(`Type strain`) %>% str_c("<b>Type strain: </b>", .)
-        
-        
-        # Check for basonyms
-        if(!is.na(search$filtered_table %>% pull(`obsolete names`))){
+        # First check whether it has been reclassified in 2020 or was already obsolete before
+        if(is.na(search$filtered_table %>% pull(`Genus`))){
             
-            # Extract all basonyms
-            additional_basonyms <- search$filtered_table %>% 
+            # Get name
+            species <- search$filtered_table %>% pull(new_name) %>% str_c("<i>", ., "</i>")
+            
+            # Get old name
+            obsolete_name <- search$filtered_table %>% 
                 pull(`obsolete names`) %>% 
-                str_replace_all(";", ",")
-            
-            # Merge with "regular" basonym
-            basonym <- search$filtered_table %>% 
-                pull(basonym)
-            
-            # If the name is not changed, display this
-            if(basonym == (search$filtered_table %>% pull(new_name))){
-                basonym <- str_c("<b>Basonyms (old species names): </b>", "<i>", additional_basonyms, "</i>")
-            } else {
-                basonym <- basonym %>% 
-                    str_c("<b>Basonyms (old species names): </b>", "<i>", ., ", ", additional_basonyms, "</i>")
-            }
+                str_c("<b>Basonyms (old species names): </b>", "<i>",., "</i>")
                 
             
             # Get dois for rejection
@@ -79,59 +64,119 @@ shinyServer(function(input, output) {""
                 str_c(collapse = ", ")
             
             # Add to string
-            basonym <- basonym %>% 
+            obsolete_name <- obsolete_name %>% 
                 str_c(.,
                       "<br/> <b>DOI for name rejection: </b>", dois)
             
+            HTML(str_c("<hr>",
+                       "<h1>",
+                       species,
+                       "<hr>",
+                       "</h1>",
+                       "This species name was obsolete prior to the 2020 reclasssification.",
+                       "<br/><br/>",
+                       obsolete_name
+                       )
+                 )
             
             
-        } else {
+        } else{
             
-            basonym <- search$filtered_table %>% 
-            pull(basonym) 
             
-            # If the name is not changed, display this
-            if(basonym == (search$filtered_table %>% pull(new_name))){
-                basonym <- str_c("<b>No name change</b>")
-            } else {
+            
+            
+            # Get name
+            species <- search$filtered_table %>% pull(new_name) %>% str_c("<i>", ., "</i>")
+            # Get type strains
+            typestrain <- search$filtered_table %>% pull(`Type strain`) %>% str_c("<b>Type strain: </b>", .)
+            
+            
+            
+            # Check for basonyms
+            if(!is.na(search$filtered_table %>% pull(`obsolete names`))){
+                
+                # Extract all basonyms
+                additional_basonyms <- search$filtered_table %>% 
+                    pull(`obsolete names`) %>% 
+                    str_replace_all(";", ",")
+                
+                # Merge with "regular" basonym
+                basonym <- search$filtered_table %>% 
+                    pull(basonym)
+                
+                # If the name is not changed, display this
+                if(basonym == (search$filtered_table %>% pull(new_name))){
+                    basonym <- str_c("<b>Basonyms (old species names): </b>", "<i>", additional_basonyms, "</i>")
+                } else {
+                    basonym <- basonym %>% 
+                        str_c("<b>Basonyms (old species names): </b>", "<i>", ., ", ", additional_basonyms, "</i>")
+                }
+                
+                
+                # Get dois for rejection
+                dois <- search$filtered_table %>% 
+                    pull(`reference for obsolete names`) %>% 
+                    str_split(";") %>% 
+                    unlist() %>% 
+                    str_squish() %>% 
+                    str_c('<a href="https://doi.org/', ., '", target="_blank">', ., "</a>") %>% 
+                    str_c(collapse = ", ")
+                
+                # Add to string
                 basonym <- basonym %>% 
-                    str_c("<b>Basonym (old species name): </b>", "<i>", ., "</i>")
+                    str_c(.,
+                          "<br/> <b>DOI for name rejection: </b>", dois)
+                
+                
+                
+            } else {
+                
+                basonym <- search$filtered_table %>% 
+                    pull(basonym) 
+                
+                # If the name is not changed, display this
+                if(basonym == (search$filtered_table %>% pull(new_name))){
+                    basonym <- str_c("<b>No name change</b>")
+                } else {
+                    basonym <- basonym %>% 
+                        str_c("<b>Basonym (old species name): </b>", "<i>", ., "</i>")
+                }
+                
             }
             
+            
+            # Additional information
+            year <- search$filtered_table %>% pull(`year of effective or valid publication`) %>% str_c("<b>Year of valid or effective publication: </b>", .)
+            doi <- search$filtered_table %>% 
+                pull(`valid description or effective publication (DOI of most recent or most relevant record)`) %>% 
+                str_c("<b>DOI: </b>", '<a href="https://doi.org/', ., '", target="_blank">', ., "</a>") 
+            
+            # Get accessions
+            rRNA <- search$filtered_table %>% pull(`16S rRNA accession no`) %>% 
+                str_c("<b>Genbank 16S rRNA gene accession number: </b>", '<a href="https://www.ncbi.nlm.nih.gov/nuccore/', ., '", target="_blank">', ., "</a>")
+            genome <- search$filtered_table %>% pull(`Genome Accession Number Type Strain`) %>% 
+                str_c("<b>Genbank genome accession number: </b>", '<a href="https://www.ncbi.nlm.nih.gov/nuccore/', ., '", target="_blank">', ., "</a>")
+            
+            
+            HTML(str_c("<hr>",
+                       "<h1>",
+                       species,
+                       "</h1>",
+                       "<hr>",
+                       typestrain,
+                       "<br/><br/>",
+                       basonym,
+                       "<h2>Accession numbers</h2>",
+                       rRNA,
+                       "<br/>",
+                       genome,
+                       "<h2>Additional information</h2>",
+                       year,
+                       "<br/>",
+                       doi
+            )
+            )
         }
-        
-        
-        # Additional information
-        year <- search$filtered_table %>% pull(`year of effective or valid publication`) %>% str_c("<b>Year of valid or effective publication: </b>", .)
-        doi <- search$filtered_table %>% 
-            pull(`valid description or effective publication (DOI of most recent or most relevant record)`) %>% 
-            str_c("<b>DOI: </b>", '<a href="https://doi.org/', ., '", target="_blank">', ., "</a>") 
-        
-        # Get accessions
-        rRNA <- search$filtered_table %>% pull(`16S rRNA accession no`) %>% 
-            str_c("<b>Genbank 16S rRNA gene accession number: </b>", '<a href="https://www.ncbi.nlm.nih.gov/nuccore/', ., '", target="_blank">', ., "</a>")
-        genome <- search$filtered_table %>% pull(`Genome Accession Number Type Strain`) %>% 
-            str_c("<b>Genbank genome accession number: </b>", '<a href="https://www.ncbi.nlm.nih.gov/nuccore/', ., '", target="_blank">', ., "</a>")
-        
-        
-        HTML(str_c("<hr>",
-                   "<h1>",
-                   species,
-                   "</h1>",
-                   "<hr>",
-                   typestrain,
-                   "<br/><br/>",
-                   basonym,
-                   "<h2>Accession numbers</h2>",
-                   rRNA,
-                   "<br/>",
-                   genome,
-                   "<h2>Additional information</h2>",
-                   year,
-                   "<br/>",
-                   doi
-                   )
-        )
     })
     
     
